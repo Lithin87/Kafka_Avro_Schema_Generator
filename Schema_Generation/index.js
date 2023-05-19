7bdb978996bf968546b94a890760300f48fa6a8c
Lithin Thomas <72386223+Lithin87@users.noreply.github.com>
2023-05-19 06:23:41 +0000

    added regex types & modular

--- Schema_Generation/index.js
+++ Schema_Generation/index.js
@@ -1,25 +1,82 @@
-const jsonfile = require('jsonfile')
-const avsc = require('avsc');
-var jp = require('jsonpath');
-
-
-const file = './Schema-Files/pizza-order.json'
-
-
+import jsonfile from 'jsonfile';
+import avsc from 'avsc';
+import jp from 'jsonpath';
+import regex_types from './custom_regex.js';
+const file = "Schema-Files/pizza-order.json";
 
 
 jsonfile.readFile(file).then( jsonObject => 
     {
         let inferredType = avsc.Type.forValue(jsonObject); // Infer the type 
         var schema = inferredType.schema();
+        var schema_c = {...schema};
 
+        console.dir(schema_c, { depth: null });
 
-        // console.dir(schema, { depth: null });
-
-        // console.log(Object.keys(schema.fields));
-        var extracted = jp.query(schema, '$.fields[*].type')
+        // var extracted = jp.query(schema, '$.fields[*].type')
+        // console.dir(extracted, { depth: null });
         console.log("-------------------------")
-        console.dir(extracted, { depth: null });
+        
+        console.log(regex_types);
+
+        const  func_replace = e => { 
+         
+            if(['int','string','float'].includes(e.type))
+            {
+            e.type  = { 
+            "type": "int",
+            "arg.properties": {
+                "range": {
+                    "min": 1,
+                    "max": 10
+                }
+            }
+        }  
+            }else{
+                if(e.type.items.type === "record" )
+                {
+                    // console.log("found a nexted array")
+                    let n = e.type.items.fields;
+                    let b = JSON.parse(JSON.stringify(n))
+                    b.forEach(func_replace);
+                    e.type.items.fields = b;
+                }
+            }
+        }
+        
+        
+        
+        var a = schema_c.fields;
+        let b = JSON.parse(JSON.stringify(a))
+        b.forEach(func_replace);
+
+
+
+        schema_c.fields = b;
+
+
+
+        console.dir(schema_c,  { depth: null });
+        // console.dir(mod_schema,  { depth: null });
+
+        // JSONObject songs= json.getJSONObject("songs");
+        // Iterator x = songs.keys();
+
+
+
+        // schema_c.fields[0].type = { 
+        //     "type": "int",
+        //     "arg.properties": {
+        //         "range": {
+        //             "min": 1,
+        //             "max": 10
+        //         }
+        //     }
+        // };
+
+
+        // console.dir(schema_c,{ depth: null })
+
 
     }
     ).catch(e => console.error("Error"+e))
