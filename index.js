ecccaf7a26167ee962f957e6bca9773ceac63a24
Lithin Thomas <72386223+Lithin87@users.noreply.github.com>
2023-05-28 18:36:41 +0000

    .

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import  express from 'express';
 import generate from './Avro-schema-generator.js' 
 import bodyParser from 'body-parser';
 
-// const app = express();
-// app.use(bodyParser.json());
+const app = express();
+app.use(bodyParser.json());
 
-// app.post('/', (req, res) => {   res.send(generate(req.body));   });
+app.post('/', (req, res) => {   res.send(generate(req.body));   });
 
 // app.get('/createkafka', (req, res) => {   res.send(generate(req.body));   });
 
 
-// const port = parseInt(process.env.PORT) || 8080;
-// app.listen(port);
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port);
 
 
-console.dir(generate(), { depth : null});
+// console.dir(generate(), { depth : null});
