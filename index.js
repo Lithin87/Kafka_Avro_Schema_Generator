58c16edc70935ceb9a7d52a62694d1b5a6c3cf18
Lithin Thomas <72386223+Lithin87@users.noreply.github.com>
2023-06-16 13:05:40 +0000

    .

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ import  express from 'express';
 import generate from './Avro-schema-generator.js' 
 import bodyParser from 'body-parser';
 
-const app = express();
-app.use(bodyParser.json());
-
-app.post('/', (req, res) => {   res.send(generate(req.body));   });
-
-// app.get('/createkafka', (req, res) => {   res.send(generate(req.body));   });
-
-
-const port = parseInt(process.env.PORT) || 8080;
-app.listen(port , ()=> console.log("Listening on the port : "+port));
+// const app = express();
+// app.use(bodyParser.json());
+// app.post('/', (req, res) => {   res.send(generate(req.body));   });
+// const port = parseInt(process.env.PORT) || 8080;
+// app.listen(port , ()=> console.log("Listening on the port : "+port));
 
 
-// console.dir(generate(), { depth : null});
+let str = {
+  "store_id": 7,
+  "order_lines": [
+    {
+      "product_id": 69,
+      "category": "salad",
+      "quantity": 3,
+      "unit_price": 17.07,
+      "net_price": 51.21
+    }
+  ]
+};
+
+
+console.dir(generate(str), {  depth :null });
+
+
+
+
